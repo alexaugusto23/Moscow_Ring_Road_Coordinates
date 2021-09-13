5bfee708463378813793b1f11a6a3c1c5ebfabc4
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-09-13 19:24:14 -0300

    test 2 matrix

--- matrix.py
+++ matrix.py
@@ -14,10 +14,11 @@ API_KEY = os.getenv("API_KEY")
 # destinations='ruamargarida,26'
 
 origins = '-23.5353749,-46.7625787'
-destinations = '37.842762,55.774558'
+destinations = '29.7147592,-95.4324201'
 
+regions = ['.br','.ru','.us']
 
-url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={origins}&destinations={destinations}&key={API_KEY}"
+url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={origins}&destinations={destinations}&key={API_KEY}&region={regions}"
 
 payload={}
 headers = {}
